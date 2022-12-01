869299fecf5a4d5bfc3e4e74f481fdd06cf66efa
Glitterz001 <pobadmus@gmail.com>
2022-12-01 13:55:42 -0800

    my second commit

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     if (len(sys.argv) > 1):
         for i in range(1, len(sys.argv)):
             result += (int(sys.argv[i]))
-    print("{:d}".format(result)
+    print("{:d}".format(result))
