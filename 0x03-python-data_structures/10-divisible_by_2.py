c570d6288deab5c18c0a0ec156c68953dbf6666a
Glitterz001 <pobadmus@gmail.com>
2022-12-06 13:56:53 -0800

    pelu commit

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -6,7 +6,5 @@ def divisible_by_2(my_list=[]):
             if i % 2 is 0:
                 values.append(True)
             else:
-                values.append(False)
-        return values
-
-# return [(i % 2 == 0) for i in my_list]
+                values.append(false)
+        return values   
