5ccfc87741cbf86d4fd17a74b69b639b23af850f
Glitterz001 <pobadmus@gmail.com>
2023-01-02 04:31:35 -0800

    my second commit

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module 1-Rectangle
+Defines class Rectangle with private attribute size
+"""
+
+
+class Rectangle:
+    """
+    class Rectangle definition
+    Args:
+        size : size of a side in rectangle
+    """
+    def __init__(self, size):
+        """
+        Initializes rectangle
+        Attributes:
+            size: size of a side of rectangle
+        """
+        self.__size = size
