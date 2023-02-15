b3b2dd07cb5a3671fb8a4b0a05e1f98a2b4494b8
Glitterz001 <pobadmus@gmail.com>
2023-02-15 09:04:18 -0800

    my fourth commit

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,7 +4,7 @@
 --      curl [link] -s | mysql -uroot -p hbtn_0d_tvshows
 --
 -- script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
--- each record should display: tv_genres.name - number_shows
+-- each record should display: Tv_genres.genre - Number_shows_linked_to_this_genre
 -- don’t display a genre that doesn’t have any shows linked
 -- results must be sorted in descending order by the number of shows linked
 -- can use only one SELECT statement
